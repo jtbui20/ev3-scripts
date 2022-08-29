df086d4e2f87f7cecdbd59bfdf4864b3f2939f57
James Bui <jtbui20@gmail.com>
2022-08-29 19:02:22 +1000

    Adjusted Files & Fixed implementations

--- robots/BaseRobot/Base_Robot.py
+++ robots/BaseRobot/Base_Robot.py
@@ -7,12 +7,13 @@ from ev3dev2.sensor import Sensor, INPUT_1, INPUT_2, INPUT_3, INPUT_4
 from ev3dev2.sensor.lego import UltrasonicSensor
 # We're going to be doing a ton of math
 import math
+import numpy as np
+import time
 
-# Make a robot object and assign it some stuff
-
-class Robot:
+class Base_Robot:
   def __init__(self, Brick = False, Debug = False):
     self.hasBrick = Brick
+    self.Debug = Debug
     # If we don't have a brick, don't do brick associated stuff
     if not Brick:
       return
@@ -23,18 +24,26 @@ class Robot:
     self._OC = LargeMotor(OUTPUT_C);
     self._OD = LargeMotor(OUTPUT_D);
 
-    if Debug:
+    if self.Debug:
       print("Motors are online")
-
 
     # These are fields for our input sensors
     self.USonW = UltrasonicSensor(INPUT_1);
+    self.USonW.mode = self.USonW.MODE_US_DIST_CM
     self.USonH = UltrasonicSensor(INPUT_4);
-    self.IRSen = Sensor(INPUT_2, driver_name="ht-nxt-ir-seek-v2")
+    self.USonH.mode = self.USonH.MODE_US_DIST_CM
+    self.CPSen = Sensor(INPUT_2, driver_name="ht-nxt-compass")
+    self.IRSen = Sensor(INPUT_3, driver_name="ht-nxt-ir-seek-v2")
     self.IRSen.mode = "AC-ALL"
 
     if Debug:
       print("Sensors are online")
+
+  '''Calibration Process'''
+  def Calibrate(self, length):
+    self.CPSen.command("BEGIN-CAL")
+    time.sleep(length)
+    self.CPSen.command("END-CAL")
 
   '''Takes an input angle and moves in that direction'''
   def RadialMove(self, angle, speed = 100):
@@ -43,13 +52,17 @@ class Robot:
     # Build an array of the values
     values = [0, 0, 0, 0]
     for i in range(0, 4):
-      values[i] = math.cos(theta - (-((2 * i ) + 1) * math.pi / 4))
+      values[i] = math.cos(theta + (((2 * i ) + 1) * math.pi / 4))
       # Round to make it easier to work with
       values[i] = round(values[i], 4)
     
     # Amp it up to the speed that we want
-    amp_ratio = (speed / 100) / max(values)
+    amp_ratio = speed/ max(values)
     values = [amp_ratio * x for x in values]
+    values = np.clip(values, -100, 100)
+
+    if self.Debug:
+      print(values)
 
     # Now we assign it to the motors
     if self.hasBrick:
@@ -58,7 +71,8 @@ class Robot:
       self._OC.on(values[2])
       self._OD.on(values[3])
 
-if __name__ == "__main__":
-  R = Robot(Brick=False, Debug=True);
-  R.RadialMove(120);
-  
+  def Stop(self):
+    self._OD.off()  
+    self._OC.off()
+    self._OB.off()
+    self._OA.off()
--- robots/Prototype Bot/code.py
+++ robots/Prototype Bot/code.py
@@ -8,90 +8,18 @@ from ev3dev2.sensor.lego import UltrasonicSensor
 # We're going to be doing a ton of math
 import math
 import numpy as np
-# More math!
-from bezier import Bezier, Progression
-
 import time
 # Ev3sim dependency
-from ev3sim.code_helpers import wait_for_tick, CommandSystem
+from ev3sim.code_helpers import wait_for_tick
+
 # Make a robot object and assign it some stuff
+from BaseRobot.Base_Robot import Base_Robot
 
-
-class Robot:
-  def __init__(self, Brick = False, Debug = False):
-    self.hasBrick = Brick
-    self.Debug = Debug
-    # If we don't have a brick, don't do brick associated stuff
-    if not Brick:
-      return
-
-    # These are fields for our output motors
-    self._OA = LargeMotor(OUTPUT_A);
-    self._OB = LargeMotor(OUTPUT_B);
-    self._OC = LargeMotor(OUTPUT_C);
-    self._OD = LargeMotor(OUTPUT_D);
-
-    if self.Debug:
-      print("Motors are online")
-
-    # These are fields for our input sensors
-    self.USonW = UltrasonicSensor(INPUT_1);
-    self.USonW.mode = self.USonW.MODE_US_DIST_CM
-    self.USonH = UltrasonicSensor(INPUT_4);
-    self.USonH.mode = self.USonH.MODE_US_DIST_CM
-    self.IRSen = Sensor(INPUT_3, driver_name="ht-nxt-ir-seek-v2")
-    self.IRSen.mode = "AC-ALL"
-
-    if Debug:
-      print("Sensors are online")
-
-  '''Takes an input angle and moves in that direction'''
-  def RadialMove(self, angle, speed = 100):
-    # Convert angle from degrees to radian
-    theta = math.radians(angle);
-    # Build an array of the values
-    values = [0, 0, 0, 0]
-    for i in range(0, 4):
-      values[i] = math.cos(theta + (((2 * i ) + 1) * math.pi / 4))
-      # Round to make it easier to work with
-      values[i] = round(values[i], 4)
-    
-    # Amp it up to the speed that we want
-    amp_ratio = speed/ max(values)
-    values = [amp_ratio * x for x in values]
-    values = np.clip(values, -100, 100)
-
-    if self.Debug:
-      print(values)
-
-    # Now we assign it to the motors
-    if self.hasBrick:
-      self._OA.on(values[0])
-      self._OB.on(values[1])
-      self._OC.on(values[2])
-      self._OD.on(values[3])
-
-  def Stop(self):
-    self._OD.off()  
-    self._OC.off()
-    self._OB.off()
-    self._OA.off()
-
+class Robot(Base_Robot):
+  pass
 
 if __name__ == "__main__":
-  R = Robot(Brick=True, Debug=False);
-  # Generate a path that we want to follow
-  points = np.array([
-    (0,0),
-    (-0.8,3),
-    (1,0),
-    (1,1)
-  ])
-  path = Bezier.Curve(np.linspace(0,1,31), points)
-  angles = Progression(path)
-
-  for i in angles:
-    R.RadialMove(i)
-    wait_for_tick()
-  R.Stop()
-  
+  R = Robot(Brick=True, Debug=False)
+  while True:
+    print(R.USonH.value(), R.USonH.value())
+    time.sleep(1)
