e601231a4faf6f03813fb8c064e49b3c8aff31e1
James Bui <jtbui20@gmail.com>
2023-06-17 22:46:23 +1000

    Search and Destroy - Solution 1

--- custom/Search and Destroy/bot/code.py
+++ custom/Search and Destroy/bot/code.py
@@ -1,4 +1,4 @@
-from ev3dev2.motor import LargeMotor, OUTPUT_A, OUTPUT_B
+# Strategy 1: Move along the axis (no rotation)
 
 x = int(input())
 y = int(input())
@@ -6,12 +6,32 @@ y = int(input())
 print(f"Green X={x}")
 print(f"Green Y={y}")
 
-# You'll need to add the motors onto the bot.
-motor1 = LargeMotor(OUTPUT_A)
-motor2 = LargeMotor(OUTPUT_B)
+# Pick which motors we want to use
+from ev3dev2.motor import LargeMotor, OUTPUT_A, OUTPUT_B
+
+horizontalMotor = LargeMotor(OUTPUT_A)
+verticalMotor = LargeMotor(OUTPUT_B)
+
+# Record some information from the brief.
+initialSpawnPoint_x = -50
+initialSpawnPoint_y = 35
+
+# Speed is found by testing how long it takes to move from right side to the middle (or top to middle)
+# This is probably the longest part of the tasks
+timeTakenToMiddle = 1.9
+
+# Use distance / time = speed
+speed = 50 / timeTakenToMiddle
+
+# Determine how far we need to move
+distanceToRight = x - initialSpawnPoint_x
+# We are going in the opposite direction so we need to multiply by -1 (positive -> negative)
+distanceToDown = (y - initialSpawnPoint_y) * -1 
 
 # Step 1: Move to the right.
+# We can also do non-blocking to move both motors at the same time (change the boolean below)
+useBlocking = True
+horizontalMotor.on_for_seconds(100, distanceToRight / speed, block=useBlocking)
 
-# Step 2: Rotate to face downards.
-
-# Step 3: Move down to the flag at position x, y
+# Step 2: Move down to the flag at position x, y.
+verticalMotor.on_for_seconds(100, distanceToDown / speed)
