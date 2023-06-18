162759a22746bbe05dd779a1995cb813f2df7552
James Bui <jtbui20@gmail.com>
2023-06-18 21:09:11 +1000

    Arrow Maze - WIP

--- custom/Arrow Maze/bot/code.py
+++ custom/Arrow Maze/bot/code.py
@@ -23,8 +23,57 @@ def forward():
     topMotor.on_for_seconds(20, 1.9)
     wait()
 
+
 # Your code goes here
 print("Grid is", grid)
-forward()
-rotate(-1)
-forward()
+new_grid = []
+for row in grid:
+    row_array = []
+    for column in row:
+        row_array.append(column)
+    new_grid.append(row_array)
+    print(row_array)
+
+# Replace the old grid with the cooler grid
+grid = new_grid
+# We ALWAYS start at 0,0
+x = 0
+y = 0
+
+old_command = "R"
+
+while True:
+    command = grid[y][x]
+    print(grid[y][x])
+    if command == "R":
+        if old_command == "U":
+            rotate(1)
+        elif old_command == "D":
+            rotate(-1)
+        forward()
+        x += 1
+    elif command == "D":
+        if old_command == "R":
+            rotate(1)
+        elif old_command == "L":
+            rotate(-1)
+        forward()
+        y += 1
+    elif command == "L":
+        if old_command == "D":
+            rotate(1)
+        elif old_command == "U":
+            rotate(-1)
+        forward()
+        x -= 1
+    elif command == "U":
+        if old_command == "L":
+            rotate(1)
+        elif old_command == "R":
+            rotate(-1)
+        forward()
+        y -= 1
+    else:
+        break
+
+    old_command = command
