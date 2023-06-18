ff6866437ea6a5c49b0b6a0f94695e1d18a04fe4
James Bui <jtbui20@gmail.com>
2023-06-18 21:07:17 +1000

    Memory Maze - Solution 2

--- custom/Memory Maze/bot/code.py
+++ custom/Memory Maze/bot/code.py
@@ -5,11 +5,46 @@ from ev3sim.code_helpers import CommandSystem
 
 leftMotor = LargeMotor("outA")
 rightMotor = LargeMotor("outB")
-color = ColorSensor("in1")
+colourSensor = ColorSensor("in1")
 ultrasonic = UltrasonicSensor("in2")
 
-# Here's some helper functions to reduce some of the trial and error.
-# Jump to the bottom of the file to seem how to use them.
+# Declare our variables
+instructions = {
+    "red": {
+        "wall": ["L", "L", "R"],
+        "empty": ["L", "R"]
+    },
+    "green": {
+        "wall": ["R", "L"],
+        "empty": ["F"]
+    },
+    "blue": {
+        "wall": ["R", "R", "L", "L"],
+        "empty": ["F", "R"]
+    }
+}
+
+counters = {
+    "red": {
+        "wall": 0,
+        "empty": 0
+    },
+    "green": {
+        "wall": 0,
+        "empty": 0
+    },
+    "blue": {
+        "wall": 0,
+        "empty": 0
+    }
+}
+
+# Green Wall    |  RLLLLLLL...
+# Red Wall      |  LLRLLR...
+# Blue Wall     |  RRLLRRLL...
+# Green No Wall |  FFF...
+# Red No Wall   |  LRLRLR...
+# Blue No Wall  |  FRFRFR...
 
 def wait():
     time.sleep(0.1)
@@ -25,87 +60,44 @@ def forward():
     rightMotor.on_for_seconds(20, 1.9, block=False)
     leftMotor.on_for_seconds(20, 1.9)
     wait()
+    
+def processInstructions(colour, isWall):
+    wallString = "wall" if isWall else "empty"
+    instruction = instructions[colour][wallString]
+    counter = counters[colour][wallString]
 
-def doInstruction(command):
+    command = instruction[counter]
     if command == "L": rotate(-1)
     elif command == "R": rotate(1)
     forward()
 
-# Let's copy paste the instructions ...
-
-# Green Wall    |  RLLLLLLL...
-# Red Wall      |  LLRLLR...
-# Blue Wall     |  RRLLRRLL...
-# Green No Wall |  FFF...
-# Red No Wall   |  LRLRLR...
-# Blue No Wall  |  FRFRFR...
-
-# ... and let the robot know what they are
-
-instructions_green_wall = ["R", "L"]
-instructions_red_wall = ["L", "L", "R"]
-instructions_blue_wall = ["R", "R", "L", "L"]
-instructions_green_empty = ["F"]
-instructions_red_empty = ["L", "R"]
-instructions_blue_empty = ["F", "R"]
-
-# Let's make some counters
-green_wall = 0
-red_wall = 0
-blue_wall = 0
-green_empty = 0
-red_empty = 0
-blue_empty = 0
+    counter += 1
+    if (counter >= len(instruction)): 
+        if (colour, wallString) != ("green", "wall"): counter = 0
+        else: counter -= 1
+    counters[colour][wallString] = counter
 
 password = ""
 
 while True:
-    r, g, b = color.rgb
-    DistanceToWall = ultrasonic.distance_centimeters
-
-    isWall = DistanceToWall <= 5
-    
+    colorFound = None
+    r,g,b = colourSensor.rgb
     if r + g + b >= 300: break
-    elif r >= 200:
+    elif r >= 200: 
+        colorFound = "red"
         password += "R"
-        if isWall:
-            doInstruction(instructions_red_wall[red_wall])
-            if red_wall == 2:
-                red_wall = 0
-            else:
-                red_wall += 1
-        else:
-            doInstruction(instructions_red_empty[red_empty])
-            if red_empty == 1:
-                red_empty = 0
-            else:
-                red_empty += 1
-    elif g >= 200:
+    elif g >= 200: 
+        colorFound = "green"
         password += "G"
-        if isWall:
-            doInstruction(instructions_green_wall[green_wall])
-            if green_wall == 0:
-                green_wall += 1
-        else:
-            doInstruction(instructions_green_empty[green_empty])
-    elif b >= 200:
+    elif b >= 200: 
+        colorFound = "blue"
         password += "B"
-        if isWall:
-            doInstruction(instructions_blue_wall[blue_wall])
-            if blue_wall == 3:
-                blue_wall = 0
-            else:
-                blue_wall += 1
-        else:
-            doInstruction(instructions_blue_empty[blue_empty])
-            if blue_empty == 1:
-                blue_empty = 0
-            else:
-                blue_empty += 1
     else:
         break
 
-    print(password)
+    isWall = ultrasonic.distance_centimeters <= 5
+
+    if colorFound in ["red", "green", "blue"]: processInstructions(colorFound, isWall)
 
 print(f"Password is {password}")
 CommandSystem.send_command(CommandSystem.TYPE_CUSTOM, password)
